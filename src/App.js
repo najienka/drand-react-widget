fa7c7e6bfa246511237ec6dda283003a3bade67f
Nemitari Ajienka <nemitariajienka@gmail.com>
2024-06-27 19:39:58 +0100

    feat: add beacon network selection

--- src/App.js
+++ src/App.js
@@ -1,71 +1,64 @@
-import React, { useState, useLayoutEffect } from 'react';
-import { 
-  // fetchBeacon, 
-  // fetchBeaconByTime, 
-  // HttpChainClient, 
-  watch, 
-  // HttpCachingChain, 
-  FastestNodeClient, 
-  // MultiBeaconNode 
-} from 'drand-client'
-
-import "./App.css"
+import React, { useState, useEffect } from 'react';
+import {
+  watch,
+  FastestNodeClient
+} from 'drand-client';
 
 // BigInt isues and solutions: https://github.com/eslint/eslint/issues/11524
 /* global BigInt */
 
 function App() {
   const [latestRandomness, setLatestRandomness] = useState(null);
-  const [eta, setETA] = useState(null)
-  const [round, setRound] = useState(null)
+  const [eta, setETA] = useState(null);
+  const [round, setRound] = useState(null);
+  const [network, setNetwork] = useState('default'); // default to 'default' network
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     const fetchLatestRandomness = async () => {
-      
       try {
-        const chainHash = '8990e7a9aaed2ffed73dbd7092123d6f289930540d7651336225dc172e51b2ce' // (hex encoded)
-        const publicKey = '868f005eb8e6e4ca0a47c8a77ceaa5309a47978a7c71bc5cce96366b5d7a569937c529eeda66c7293784a9402801af31' // (hex encoded)
+        let chainHash, publicKey;
+        let updateInterval = 30 * 1000; // default update interval in milliseconds
 
-        const options = {
-          disableBeaconVerification: false, // `true` disables checking of signatures on beacons - faster but insecure!!!
-          noCache: false, // `true` disables caching when retrieving beacons for some providers
-          chainVerificationParams: { chainHash, publicKey }  // these are optional, but recommended! They are compared for parity against the `/info` output of a given node
+        // Determine chainHash, publicKey, and updateInterval based on selected network
+        if (network === 'default') {
+          chainHash = '8990e7a9aaed2ffed73dbd7092123d6f289930540d7651336225dc172e51b2ce'; // mainnet default
+          publicKey = '868f005eb8e6e4ca0a47c8a77ceaa5309a47978a7c71bc5cce96366b5d7a569937c529eeda66c7293784a9402801af31';
+        } else if (network === 'quicknet') {
+          chainHash = '52db9ba70e0cc0f6eaf7803dd07447a1f5477735fd3f661792ba94600c84e971'; // mainnet quicknet
+          publicKey = '83cf0f2896adee7eb8b5f01fcad3912212c437e0073e911fb90022d3e760183c8c4b450b6a0a6c3ac6a5776a2d1064510d1fec758c921cc22b0e17e63aaf4bcb5ed66304de9cf809bd274ca73bab4af5a6e9c76a4bc09e76eae8991ef5ece45a';
+          updateInterval = 3 * 1000; // quicknet update interval in milliseconds (3 seconds)
         }
 
-        
-        // fetching randomness from multiple APIs and automatically use the fastest
-        const urls = [
-          "https://api.drand.sh",
-          "https://api2.drand.sh",
-          "https://api3.drand.sh",
-          "https://drand.cloudflare.com"
-        ]
-        
-        const client = new FastestNodeClient(urls, options)
+        const options = {
+          disableBeaconVerification: false,
+          noCache: false,
+          chainVerificationParams: { chainHash, publicKey }
+        };
 
-        // start the client, so that it periodically optimises for the fastest node!
-        client.start()
+        let urls = [
+          'http://api.drand.sh',
+          'http://api2.drand.sh',
+          'http://api3.drand.sh',
+          'http://drand.cloudflare.com'
+        ];
 
-        // const info = await client.info()
+        // Append chainHash to each URL if quicknet is selected
+        if (network === 'quicknet') {
+          urls = urls.map(url => `${url}/${chainHash}`);
+        }
 
-        // todo: we could automate fetching of eta
-        // const nextRoundETA = () => {
-        //   const now = Date.now()
-        //   const round = // current beacon round
-        //   const time = info.genesis_time * 1000 + round * info.period * 1000
-        //   return Math.round((time - now) / 1000)
-        // }
+        const headers = {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': 'true'};
+        const client = new FastestNodeClient(urls, options, {headers});
 
-        // client.stop()
+        client.start();
 
-        const abortController = new AbortController()
+        const abortController = new AbortController();
         for await (const beacon of watch(client, abortController)) {
-            const updateInterval = 30 * 1000; // 30 seconds in milliseconds
-            const currentTime = new Date().getTime();
-            const nextETA = new Date(currentTime + updateInterval);
-            setETA(nextETA.toLocaleTimeString());
-            setRound(beacon.round);
-            setLatestRandomness(BigInt(`0x${beacon.randomness}`).toString());
+          const currentTime = new Date().getTime();
+          const nextETA = new Date(currentTime + updateInterval);
+          setETA(nextETA.toLocaleTimeString());
+          setRound(beacon.round);
+          setLatestRandomness(BigInt(`0x${beacon.randomness}`).toString());
         }
       } catch (error) {
         console.error('Error fetching randomness:', error);
@@ -73,23 +66,44 @@ function App() {
     };
 
     fetchLatestRandomness();
-  }, []);
+  }, [network]);
+
+  const handleNetworkChange = (event) => {
+    setNetwork(event.target.value);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Latest Randomness from dRAND Beacon</h1>
-        <div className="info">
-          <div className="info-item">
-            <strong>Next Randomness ETA:</strong> <span>{eta ? eta : 'Loading...'}</span>
-          </div>
-          <div className="info-item">
-            <strong>Latest Round:</strong> <span>{round ? round : 'Loading...'}</span>
-          </div>
-          <div className="info-item">
-            <strong>Latest Randomness:</strong> <span>{latestRandomness ? latestRandomness : 'Loading...'}</span>
-          </div>
+        <h1>Latest Randomness from drand Beacon</h1>
+
+        <div>
+          Select Beacon Chain:
+          <select value={network} onChange={handleNetworkChange} style={{ marginLeft: '10px' }}>
+            <option value="default">Default</option>
+            <option value="quicknet">Quicknet</option>
+          </select>
         </div>
+
+        <br></br>
+
+        Next Randomness ETA: {eta ? (
+          <p>{eta}</p>
+        ) : (
+          <p>Loading...</p>
+        )}
+
+        Latest Round: {round ? (
+          <p>{round}</p>
+        ) : (
+          <p>Loading...</p>
+        )}
+
+        Latest Randomness: {latestRandomness ? (
+          <p>{latestRandomness}</p>
+        ) : (
+          <p>Loading...</p>
+        )}
       </header>
     </div>
   );
